ff52267001978960c35704009eed6af28293ce19
szne <me@szn.jp>
2025-09-23 15:52:17 +0900

    📦️ Package: @tailwindcss/typographyを導入

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+import typography from "@tailwindcss/typography";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [typography],
+};
